3ee2a665d1d5fe715f102293e236fe32e3489833
gcrew <gurpreetgill42@gmail.com>
2017-01-19 13:05:50 -0500

    lmao

--- Scripts/background.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('myButton').addEventListener('click', function() {
-	chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.executeScript(tab.id, {file: "./Scripts/content_script.js"});
-  });
-});
