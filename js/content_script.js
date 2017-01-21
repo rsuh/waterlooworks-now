e7575e5254dad469f283d622c98d59b785f54a02
gcrew <gurpreetgill42@gmail.com>
2017-01-21 18:57:13 -0500

    getting html works

--- js/content_script.js
+++ js/content_script.js
@@ -1,5 +1,3 @@
-const baseWaterlooWorksUrl = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm";
-
 /* This function returns the html without opening a new tab for the page
  * accessed the param url. It is assumed that for WaterlooWorks the url will have
  * the format like <baseWaterlooWorksUrl>?<QueryParam>
@@ -8,14 +6,37 @@ const baseWaterlooWorksUrl = "https://waterlooworks.uwaterloo.ca/myAccount/co-op
  * @return {String} - string defining the html of the page */
 function getJobPostingHTML(url) {
 
-  var htmlObject = null;
+  return $.get(url, function(firsthtml) {
+    var parsedFirstHtml = $.parseHTML(firsthtml);
 
-  $.get(link, function(html) {
-      htmlObject = html;
-  });
+    // An array of objects of format: {name: <name>, value: <value>}
+    var inputNameValues = new Object;
 
-  // WaterlooWorks submits this form and then redirects you to the posting. We will have to make
-  // the form request
+    $.each(parsedFirstHtml[0], function(i, el) {
+      inputNameValues[el.name] = el.value;
+    });
+
+    return inputNameValues;
+  })
+  .then((value) => {
+    return $.ajax({
+      type: "POST",
+      url: "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm",
+      data: value
+    })
+  })
 
 }
+
+const baseWaterlooWorksUrl = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm";
+var testurl = baseWaterlooWorksUrl + "?action=_-_-xps-CTTTkwP8nQhDoJIOewbWnqrPuPQRFrDd44kyFmhUvbQuOeYbflR2oDiXWCHcVP-yKde07ohfviclaCOPCWOGX-p3HYVMdVhYeL0uqc8DWUdZ7IZFPq4K3A5a5ASN3lzb_WdIIpcA3e0uLeJ-L7vsShVCWtiE995ywlq-jA&initialSearchAction=displayMyProgramJobs&searchType=&accessToPostings=infoForPostings&postingId=5048&npfGroup=&sortDirection=Reverse";
+
+//console.log(getJobPostingHTML(testurl));
+
+getJobPostingHTML(testurl).then((result) => {
+  console.log(result);
+});
+
+
+// console.log(Object.keys(getJobPostingHTML(testurl)));
 
