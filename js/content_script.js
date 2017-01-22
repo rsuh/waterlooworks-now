461a367c6d35e2e94358f21a0c482ea5cbd58645
gcrew <gurpreetgill42@gmail.com>
2017-01-22 00:09:10 -0500

    finished the function which grabs the html

--- js/content_script.js
+++ js/content_script.js
@@ -6,37 +6,29 @@
  * @return {String} - string defining the html of the page */
 function getJobPostingHTML(url) {
 
-  return $.get(url, function(firsthtml) {
-    var parsedFirstHtml = $.parseHTML(firsthtml);
-
-    // An array of objects of format: {name: <name>, value: <value>}
+  return $.get(url, function(html) {
+    return html;
+  })
+  .then((initialHTML) => {
+    var parsedInitialHtml = $.parseHTML(initialHTML);
     var inputNameValues = new Object;
 
-    $.each(parsedFirstHtml[0], function(i, el) {
+    $.each(parsedInitialHtml[0], function(i, el) {
       inputNameValues[el.name] = el.value;
     });
 
-    return inputNameValues;
-  })
-  .then((value) => {
     return $.ajax({
       type: "POST",
       url: "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm",
-      data: value
-    })
+      data: inputNameValues
+    });
   })
 
 }
 
-const baseWaterlooWorksUrl = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm";
-var testurl = baseWaterlooWorksUrl + "?action=_-_-xps-CTTTkwP8nQhDoJIOewbWnqrPuPQRFrDd44kyFmhUvbQuOeYbflR2oDiXWCHcVP-yKde07ohfviclaCOPCWOGX-p3HYVMdVhYeL0uqc8DWUdZ7IZFPq4K3A5a5ASN3lzb_WdIIpcA3e0uLeJ-L7vsShVCWtiE995ywlq-jA&initialSearchAction=displayMyProgramJobs&searchType=&accessToPostings=infoForPostings&postingId=5048&npfGroup=&sortDirection=Reverse";
+// const baseWaterlooWorksUrl = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm";
+// var testurl = baseWaterlooWorksUrl + "?action=_-_-xps-CTTTkwP8nQhDoJIOewbWnqrPuPQRFrDd44kyFmhUvbQuOeYbflR2oDiXWCHcVP-yKde07ohfviclaCOPCWOGX-p3HYVMdVhYeL0uqc8DWUdZ7IZFPq4K3A5a5ASN3lzb_WdIIpcA3e0uLeJ-L7vsShVCWtiE995ywlq-jA&initialSearchAction=displayMyProgramJobs&searchType=&accessToPostings=infoForPostings&postingId=5048&npfGroup=&sortDirection=Reverse";
 
-//console.log(getJobPostingHTML(testurl));
-
-getJobPostingHTML(testurl).then((result) => {
-  console.log(result);
-});
-
-
-// console.log(Object.keys(getJobPostingHTML(testurl)));
-
+// getJobPostingHTML(testurl).then((result) => {
+//   console.log(result);
+// });
